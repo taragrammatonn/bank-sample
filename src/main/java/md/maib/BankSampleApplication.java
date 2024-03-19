745d76ad5fa11e281ab84583fec392df3dc69691
Mihai <potoracmihai74@gmail.com>
2024-03-19 15:28:30 +0200

    Project Structure

---
+++ src/main/java/md/maib/BankSampleApplication.java
@@ -0,0 +1,13 @@
+package md.maib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSampleApplication.class, args);
+    }
+
+}
