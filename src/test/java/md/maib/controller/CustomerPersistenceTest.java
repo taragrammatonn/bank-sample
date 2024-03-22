6b0e49e67461dac1947bb8be2d6f6ff6719581cb
jah3 <isecov9@gmail.com>
2024-03-22 16:01:25 +0200

    Contract for Controller and Database Interaction

--- src/test/java/md/maib/controller/CustomerPersistenceTest.java
+++ src/test/java/md/maib/controller/CustomerPersistenceTest.java
@@ -2,7 +2,6 @@ package md.maib.controller;
 
 import md.maib.entity.Customer;
 import md.maib.repository.CustomerRepository;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,6 +28,8 @@ class CustomerPersistenceTest {
 
     private final CustomerRepository customerRepository;
 
+    private Customer savedCustomer = null;
+
     public CustomerPersistenceTest(@Autowired CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
@@ -51,13 +52,21 @@ class CustomerPersistenceTest {
     }
 
     @BeforeEach
-    void beforeEach() {
+    public void initCustomer() {
         customerRepository.deleteAll();
+        savedCustomer = new Customer.Builder()
+                .firstName("John")
+                .lastName("Doe")
+                .pan("1234567891011121")
+                .cvv("123")
+                .build();
+
+        customerRepository.save(savedCustomer);
     }
 
     @Test
     void shouldSelectCustomerById() {
-        var savedCustomer = initCustomers();
+        var savedCustomer = this.savedCustomer;
 
         var customerId = savedCustomer.getId();
         var retrievedCustomer = customerRepository.findById(customerId)
@@ -68,7 +77,7 @@ class CustomerPersistenceTest {
 
     @Test
     void shouldUpdateCustomerDetails() {
-        var savedCustomer = initCustomers();
+        var savedCustomer = this.savedCustomer;
 
         savedCustomer.setFirstName("UpdatedAlice");
         savedCustomer.setLastName("UpdatedSmith");
@@ -80,7 +89,7 @@ class CustomerPersistenceTest {
 
     @Test
     void shouldInsertNewCustomer() {
-        var savedCustomer = initCustomer();
+        var savedCustomer = this.savedCustomer;
 
         assertNotNull(savedCustomer.getId());
         assertEquals("John", savedCustomer.getFirstName());
@@ -89,54 +98,20 @@ class CustomerPersistenceTest {
         assertEquals("123", savedCustomer.getCvv());
     }
 
-    @NotNull
-    private Customer initCustomer() {
-        final var customer = new Customer.Builder()
-                .firstName("John")
-                .lastName("Doe")
-                .pan("1234567891011121")
-                .cvv("123")
-                .build();
 
-        final var savedCustomer = customerRepository.save(customer);
-        return savedCustomer;
-    }
 
     @Test
     void shouldNotSaveCustomerWithDuplicatePan() {
 
-        extracted();
-
         var customer2 = new Customer.Builder()
                 .firstName("Bob")
                 .lastName("Johnson")
-                .pan("1234567891011122")
+                .pan("1234567891011121")
                 .cvv("789")
                 .build();
 
         assertThrows(DataIntegrityViolationException.class, () -> {
             customerRepository.save(customer2);
         });
-    }
-
-    private void extracted() {
-        var customer1 = new Customer.Builder()
-                .firstName("Alice")
-                .lastName("Smith")
-                .pan("1234567891011122")
-                .cvv("456")
-                .build();
-        customerRepository.save(customer1);
-    }
-
-    private Customer initCustomers() {
-        var customer = new Customer.Builder()
-                .firstName("Alice")
-                .lastName("Smith")
-                .pan("1234567891011122")
-                .cvv("456")
-                .build();
-        var savedCustomer = customerRepository.save(customer);
-        return savedCustomer;
     }
 }
