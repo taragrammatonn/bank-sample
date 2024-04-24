453f751b27d0a942d18e701df8824c2420d6418b
jah3 <isecov9@gmail.com>
2024-04-24 10:34:03 +0300

    Encrypt Auth

--- src/test/java/md/maib/bank/sample/entity/PhoneNumberTest.java
+++ src/test/java/md/maib/bank/sample/entity/PhoneNumberTest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
 @Testcontainers
 class PhoneNumberTest extends AbstractContainerBaseTest {
 
